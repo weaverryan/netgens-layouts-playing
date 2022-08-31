6044c5a6fe120852f08f0ee027235bd1ad12268f
Ryan Weaver <ryan@thatsquality.com>
2022-08-31 15:25:35 -0400

    tuts hero working...

--- config/bundles.php
+++ config/bundles.php
@@ -22,4 +22,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
