6078670bc7d42f7734d87c2714efecd3d46b26ed
Ryan Weaver <ryan@thatsquality.com>
2022-08-31 14:40:19 -0400

    tuts hero working...

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,23 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('ryan@example.com');
+        $user->setPassword($this->passwordHasher->hashPassword('admin'));
+        $manager->persist($user);
 
         $manager->flush();
     }
