9756b452b13626c8c79d651e306117baad4f391f
Ryan Weaver <ryan@thatsquality.com>
2022-08-31 15:08:00 -0400

    tuts hero working...

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
