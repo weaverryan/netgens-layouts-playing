950dd6eae22ab7cafa32f08e28369ad2d14bb581
Ryan Weaver <ryan@thatsquality.com>
2022-09-08 14:57:29 -0400

    tuts hero working...

--- src/Layouts/ScreencastValueUrlGenerator.php
+++ src/Layouts/ScreencastValueUrlGenerator.php
@@ -2,12 +2,22 @@
 
 namespace App\Layouts;
 
+use App\Entity\Screencast;
 use Netgen\Layouts\Item\ValueUrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 
-class ScreencastUrlGenerator implements ValueUrlGeneratorInterface
+class ScreencastValueUrlGenerator implements ValueUrlGeneratorInterface
 {
+    public function __construct(private RouterInterface $router)
+    {
+    }
+
     public function generate(object $object): ?string
     {
-        // TODO: Implement generate() method.
+        assert($object instanceof Screencast);
+
+        return $this->router->generate('app_screencast_show', [
+            'id' => $object->getId()
+        ]);
     }
 }
