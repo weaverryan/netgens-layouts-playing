d20ac8c27c5879d87f0d1876d265ebf9be3982fc
Ryan Weaver <ryan@thatsquality.com>
2022-09-08 14:50:36 -0400

    tuts hero working...

---
+++ src/Layouts/ScreencastUrlGenerator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Layouts;
+
+use Netgen\Layouts\Item\CmsItemInterface;
+use Netgen\Layouts\Item\UrlGeneratorInterface;
+
+class ScreencastUrlGenerator implements UrlGeneratorInterface
+{
+    public function generate(CmsItemInterface $item): string
+    {
+        // TODO: Implement generate() method.
+    }
+}
