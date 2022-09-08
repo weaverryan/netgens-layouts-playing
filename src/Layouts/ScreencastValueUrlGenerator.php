07a281d6d56de40f143aeca500c6f531374ca4ac
Ryan Weaver <ryan@thatsquality.com>
2022-09-08 15:11:46 -0400

    tuts hero working...

--- src/Layouts/ScreencastValueUrlGenerator.php
+++ src/Layouts/ScreencastValueUrlGenerator.php
@@ -4,8 +4,10 @@ namespace App\Layouts;
 
 use App\Entity\Screencast;
 use Netgen\Layouts\Item\ValueUrlGeneratorInterface;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 use Symfony\Component\Routing\RouterInterface;
 
+#[AutoconfigureTag('netgen_layouts.cms_value_url_generator', ['value_type' => 'doctrine_screencast'])]
 class ScreencastValueUrlGenerator implements ValueUrlGeneratorInterface
 {
     public function __construct(private RouterInterface $router)
