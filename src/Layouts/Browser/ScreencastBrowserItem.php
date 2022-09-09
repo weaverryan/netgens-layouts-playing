17033347010a6a87f2fcaea80a2f3e51affecc7c
Ryan Weaver <ryan@thatsquality.com>
2022-09-09 14:00:53 -0400

    tuts hero working...

---
+++ src/Layouts/Browser/ScreencastBrowserItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Layouts\Browser;
+
+use Netgen\ContentBrowser\Item\ItemInterface;
+
+class ScreencastBrowserItem implements ItemInterface
+{
+    public function getValue()
+    {
+        // TODO: Implement getValue() method.
+    }
+
+    public function getName(): string
+    {
+        // TODO: Implement getName() method.
+    }
+
+    public function isVisible(): bool
+    {
+        // TODO: Implement isVisible() method.
+    }
+
+    public function isSelectable(): bool
+    {
+        // TODO: Implement isSelectable() method.
+    }
+}
