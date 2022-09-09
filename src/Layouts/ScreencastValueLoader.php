a3e8c1a779a5ad4fdec760f9817a1bde5fbacf03
Ryan Weaver <ryan@thatsquality.com>
2022-09-09 14:20:16 -0400

    tuts hero working...

--- src/Layouts/ScreencastValueLoader.php
+++ src/Layouts/ScreencastValueLoader.php
@@ -3,7 +3,9 @@
 namespace App\Layouts;
 
 use Netgen\Layouts\Item\ValueLoaderInterface;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
+#[AutoconfigureTag('netgen_layouts.cms_value_loader', ['value_type' => 'doctrine_screencast'])]
 class ScreencastValueLoader implements ValueLoaderInterface
 {
     public function load($id): ?object
