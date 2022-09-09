87653a99217e065dbd4e1d9710a019f05920b1e7
Ryan Weaver <ryan@thatsquality.com>
2022-09-09 14:18:59 -0400

    tuts hero working...

---
+++ src/Layouts/ScreencastValueLoader.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Layouts;
+
+use Netgen\Layouts\Item\ValueLoaderInterface;
+
+class ScreencastValueLoader implements ValueLoaderInterface
+{
+    public function load($id): ?object
+    {
+        // TODO: Implement load() method.
+    }
+
+    public function loadByRemoteId($remoteId): ?object
+    {
+        // TODO: Implement loadByRemoteId() method.
+    }
+}
