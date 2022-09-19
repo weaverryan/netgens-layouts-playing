0f4049e6b585d02d7f32f992e2bb1df116d3c5b8
Ryan Weaver <ryan@thatsquality.com>
2022-09-19 12:38:24 -0400

    tuts hero working...

--- src/Layouts/BlogSlugger.php
+++ src/Layouts/BlogSlugger.php
@@ -5,7 +5,9 @@ namespace App\Layouts;
 use Netgen\Layouts\Contentful\Entity\ContentfulEntry;
 use Netgen\Layouts\Contentful\Routing\EntrySlugger\FilterSlugTrait;
 use Netgen\Layouts\Contentful\Routing\EntrySluggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
+#[AutoconfigureTag('netgen_layouts.contentful.entry_slugger', ['type' => 'blogPost'])]
 class BlogSlugger implements EntrySluggerInterface
 {
     use FilterSlugTrait;
